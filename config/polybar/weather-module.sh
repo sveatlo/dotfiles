4c232fac3a92dc6b497cbb7d4b65f13f4b1e9fca
Svätopluk Hanzel <svatoplukhanzel@gmail.com>
2020-09-16 12:09:50 +0200

    polybar: [deprecated] sway-compatibility changes

--- config/polybar/weather-module.sh
+++ config/polybar/weather-module.sh
@@ -11,7 +11,7 @@ while true; do
     if [ $t -eq 0 ]; then
         ~/src/scripts/weather.py --output-format '%{T4}{{current.icon}}%{T-}  {{current.temperature}}°C'
     else
-        ~/src/scripts/weather.py --output-format '{{current.temperature}}°C, {{current.description_long}} -> {{next.temperature}}°C, {{next.description_long}}'
+        ~/src/scripts/weather.py --output-format '{{city}} - {{current.temperature}}°C, {{current.description_long}} -> {{next.temperature}}°C, {{next.description_long}}'
     fi
 
     sleep 600 &
